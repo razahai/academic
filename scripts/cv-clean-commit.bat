841d3290bd5cb244b5f26b1664978dbd0cc99d7c
raza <101937798+razahai@users.noreply.github.com>
2024-12-22 17:23:34 -0500

    fix bat cmd and hierarchy errors in scripts

--- scripts/cv-clean-commit.bat
+++ scripts/cv-clean-commit.bat
@@ -9,7 +9,7 @@ if not exist %CD%\courses\tjhsst\%folder%\ echo You need to name your CV folder 
 @REM temporarily append -original to the folder so it gets ignored by git
 @REM copy the original folder into a new folder called "cv" which will be
 @REM committed to the repository
-ren courses\tjhsst\%folder% "courses\tjhsst\%folder%-original"
+ren "courses\tjhsst\%folder%" "%folder%-original"
 robocopy "courses\tjhsst\%folder%-original" courses\tjhsst\cv /E > nul
 
 cd "./courses/tjhsst/%folder%" || exit /b 1
@@ -36,7 +36,7 @@ for /r %%f in (*) do (
 
 endlocal
 
-cd ..
+cd ../../..
 
 git add courses/tjhsst/cv > nul
 
@@ -51,4 +51,4 @@ git push origin main
 @REM delete the temporary folder and rename the original folder
 @REM back to its original name
 rd /s /q "./courses/tjhsst/%folder%"
-ren "courses\tjhsst\%folder%-original" courses\tjhsst\%folder%
+ren "courses\tjhsst\%folder%-original" %folder%
